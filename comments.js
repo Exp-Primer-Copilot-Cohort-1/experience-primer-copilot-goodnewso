aff21f3bd2c02efe7332b4eab5699f762ac35f38
Goodnews Ogboada Jaja <goodnewso@hotmail.com>
2023-10-23 18:31:42 +0100

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,4 +1,23 @@
-// Create web server
+// Create web server for comments
+//==============================================================================
 
+
+// import modules
 const express = require('express');
-const router = express. Router ();
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+
+// Handle request
+router.get('/', commentController.comment_list);
+router.get('/create', commentController.comment_create_get);
+router.post('/create', commentController.comment_create_post);
+router.get('/:id/delete', commentController.comment_delete_get);
+router.post('/:id/delete', commentController.comment_delete_post);
+router.get('/:id/update', commentController.comment_update_get);
+router.post('/:id/update', commentController.comment_update_post);
+router.get('/:id', commentController.comment_detail);
+
+
+// export module
+module.exports = router;
